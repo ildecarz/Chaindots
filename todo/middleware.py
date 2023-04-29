11c0f549bb60b48340cb90015f5b1d409f26d4af
Ildemaro Carrasco <ildemarocarrasco@gmail.com>
2023-04-29 00:35:08 -0300

    fix typo

--- todo/middleware.py
+++ todo/middleware.py
@@ -6,7 +6,7 @@ class LogMiddleware:
     def __call__(self, request):
         response = self.get_response(request)
 
-        print( f'The user: {request.user.username} has has made a request to: {request.path}, description {request.POST.get("description")}')
+        print( f'The user: {request.user.username} has made a request to: {request.path}, description {request.POST.get("description")}')
 
         return response
 
